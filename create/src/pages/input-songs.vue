3e755c2a5d669290b42bf31aafc1e0c54547f512
Rafael Gentil <rfl.gentil@gmail.com>
2020-03-22 20:22:53 -0300

    Start project vue

---
+++ create/src/pages/input-songs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Input Songs Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputSongs"
+};
+</script>
+
+<style scoped></style>
---
+++ create/src/pages/songs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Songs Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Songs"
+};
+</script>
+
+<style scoped></style>
