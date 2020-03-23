2965964a5e78f9ce6bd6fca5c1ba27e32d9041f3
Rafael Gentil <rfl.gentil@gmail.com>
2020-03-22 21:22:39 -0300

    Add Vue Material

--- create/src/main.js
+++ create/src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import router from "./router";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default-dark.css";
+
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMaterial);
 
 new Vue({
   router,
--- create/src/pages/songs.vue
+++ create/src/pages/songs.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Songs Page</h1>
+
+    <md-button class="md-primary">Primary</md-button>
   </div>
 </template>
 
